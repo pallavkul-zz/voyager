0207c918701383a86477152f9cdb3876b75953d1
Vivek Ratnavel <vivekratnavel90@gmail.com>
2015-07-31 11:16:52 -0700

    Fixed jshint warnings

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -18,22 +18,22 @@ angular.module('voyager')
     // Flag to show loading spinner
     $scope.loading = false;
 
-    if(Object.keys($location.search()).indexOf("url") >= 0) {
+    if(Object.keys($location.search()).indexOf('url') >= 0) {
         var url = $location.search().url;
         var job_id;
 
         var count = 30000; // Default fetch count
-        if(Object.keys($location.search()).indexOf("count") >= 0) {
+        if(Object.keys($location.search()).indexOf('count') >= 0) {
             count = $location.search().count;
         }
-        if(Object.keys($location.search()).indexOf("job_id") >= 0) {
+        if(Object.keys($location.search()).indexOf('job_id') >= 0) {
             job_id = $location.search().job_id;
         } else {
             job_id = Math.floor((Math.random() * 10000) + 1);
         }
         url = url.trim();
         // Remove trailing '?' in url
-        if (url.substring(url.length-1) == "?") {
+        if (url.substring(url.length-1) == '?') {
             url = url.substring(0, url.length-1);
         }
         url += '?&count=' + count + '&first=true&format=d3';
@@ -46,7 +46,7 @@ angular.module('voyager')
             if('status' in results && 'message' in results) {
                 var errorMessage = results.message;
                 // Shorten the error message
-                var maxLength = 500 // maximum number of characters
+                var maxLength = 500; // maximum number of characters
                 errorMessage = shortenText(errorMessage, maxLength);
                 Alerts.add('Error: ' + errorMessage);
             } else {
@@ -75,8 +75,8 @@ angular.module('voyager')
         if(text.length > maxLength) {
             text = text.substr(0, maxLength);
             // Re-trim if we are in the middle of a word
-            text = text.substr(0, Math.min(text.length, text.lastIndexOf(" ")));
-            text += "...";
+            text = text.substr(0, Math.min(text.length, text.lastIndexOf(' ')));
+            text += '...';
         }
         return text;
     }
